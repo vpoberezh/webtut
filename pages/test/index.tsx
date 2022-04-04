827447666ab64b02f0766df7a9369b5942bf1836
unknown <vadym.poz@inx.co>
2022-04-04 15:53:19 +0300

    empty page

---
+++ pages/test/index.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import styles from './index.module.scss';
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.root}>
+      <h1>Test</h1>
+    </div>
+  )
+}
+
+export default Home
